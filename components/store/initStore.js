c56812c5ce94251b4c3e4512f3f717da9241a16c
David Yusta <david.yusta@guidesmiths.com>
2020-09-22 21:33:30 +0200

    feat: add getByStatus, upsertOne and deleteOne. Refactor getAll

--- components/store/initStore.js
+++ components/store/initStore.js
@@ -14,25 +14,33 @@ module.exports = () => {
     const successCollection = mongo.db(databaseName).collection(success);
     const failCollection = mongo.db(databaseName).collection(fail);
 
-    const getAll = collection => async () => (await collection.find({})).toArray();
+    const getAll = collection => async (query = {}) => (await collection.find(query)).toArray();
     const getByFileName = collection => filename => collection.findOne({ filename });
+    const getByStatus = collection => async status => (await collection.find({ status })).toArray();
     const insertOne = collection => async payload => {
       const dataToInsert = JSON.stringify(payload);
       logger.info(`${collection.namespace} | Inserting new document: ${dataToInsert}`);
-      const { ops, insertedCount } = await collection.insertOne({ ...payload, retries: 0, date: new Date() });
+      const { ops, insertedCount } = await collection.insertOne({ ...payload, date: new Date() });
       if (!insertedCount) throw new Error(`${collection.namespace} | Could not save data: ${dataToInsert}`);
       return ops[0];
     };
-    const updateOne = collection => async payload => {
+    const upsertOne = collection => async payload => {
       const { _id } = payload;
-      const { value } = await collection.findOneAndUpdate({ _id }, { $set: payload }, { returnOriginal: false });
+      const updatedPayload = { ...payload, date: new Date() };
+      const { value } = await collection.findOneAndUpdate({ _id }, { $set: updatedPayload }, { returnOriginal: false, upsert: true });
       return value;
     };
-
     const deleteOneByFilename = collection => async filename => {
       logger.info(`${collection.namespace} | Removing one document with filename: ${filename}`);
       const { deletedCount } = await collection.deleteOne({ filename });
       if (!deletedCount) throw new Error(`${collection.namespace} | Could not delete document with filename ${filename}`);
+      return deletedCount;
+    };
+    const deleteOne = collection => async payload => {
+      const { filename, status } = payload;
+      logger.info(`${collection.namespace} | Removing one document | Filename ${filename} Status: ${status}`);
+      const { deletedCount } = await collection.deleteOne({ filename, status });
+      if (!deletedCount) throw new Error(`${collection.namespace} | Could not delete document | Filename ${filename} Status: ${status}`);
       return deletedCount;
     };
     const deleteAll = collection => async () => {
@@ -46,11 +54,14 @@ module.exports = () => {
     const getSuccessByFilename = getByFileName(successCollection);
     const getFailByFilename = getByFileName(failCollection);
 
+    const getSuccessByStatus = getByStatus(successCollection);
+    const getFailByStatus = getByStatus(failCollection);
+
     const insertOneSuccess = insertOne(successCollection);
     const insertOneFail = insertOne(failCollection);
 
-    const updateOneSuccess = updateOne(successCollection);
-    const updateOneFail = updateOne(failCollection);
+    const upsertOneSuccess = upsertOne(successCollection);
+    const upsertOneFail = upsertOne(failCollection);
 
     const deleteOneSuccessByFilename = deleteOneByFilename(successCollection);
     const deleteOneFailByFilename = deleteOneByFilename(failCollection);
@@ -58,19 +69,26 @@ module.exports = () => {
     const deleteAllSuccess = deleteAll(successCollection);
     const deleteAllFail = deleteAll(failCollection);
 
+    const deleteOneSuccess = deleteOne(successCollection);
+    const deleteOneFail = deleteOne(failCollection);
+
     return {
       getAllSuccess,
       getAllFail,
       getSuccessByFilename,
       getFailByFilename,
+      getSuccessByStatus,
+      getFailByStatus,
       insertOneSuccess,
       insertOneFail,
-      updateOneSuccess,
-      updateOneFail,
+      upsertOneSuccess,
+      upsertOneFail,
       deleteOneSuccessByFilename,
       deleteOneFailByFilename,
       deleteAllSuccess,
       deleteAllFail,
+      deleteOneSuccess,
+      deleteOneFail,
     };
   };
 
--- test/components/store/store.test.js
+++ test/components/store/store.test.js
@@ -36,6 +36,7 @@ describe('Store component tests', () => {
       const result = await store.insertOneSuccess({
         filename,
         statistics,
+        retries: 0,
       });
       expect(result._id).toBeDefined();
       expect(result).toMatchObject(expectedResult);
@@ -58,36 +59,55 @@ describe('Store component tests', () => {
       const result = await store.insertOneFail({
         filename,
         statistics,
+        retries: 0,
       });
       expect(result._id).toBeDefined();
       expect(result).toMatchObject(expectedResult);
     });
   });
 
-  describe('updateOne tests', () => {
-    test('should not update a document in success collection', async () => {
+  describe('upsertOne tests', () => {
+    test('should upsert (insert) a document in success collection', async () => {
       const filename = '2020-08-25';
       const statistics = {
         gnuplot: '128',
         screenshots: '100',
         stats: '80',
       };
-      const result = await store.updateOneSuccess({ filename, statistics, retries: 1 });
-      expect(result).toBeNull();
+
+      const expectedResult = {
+        filename,
+        statistics,
+        date: expect.any(Date),
+        retries: 1,
+      };
+
+      const result = await store.upsertOneSuccess({ filename, statistics, retries: 1 });
+      expect(result._id).toBeDefined();
+      expect(result).toMatchObject(expectedResult);
     });
 
-    test('should not update a document in fail collection', async () => {
+    test('should upsert (insert) a document in fail collection', async () => {
       const filename = '2020-08-25';
       const statistics = {
         gnuplot: '128',
         screenshots: '100',
         stats: '80',
       };
-      const result = await store.updateOneFail({ filename, statistics, retries: 1 });
-      expect(result).toBeNull();
+
+      const expectedResult = {
+        filename,
+        statistics,
+        date: expect.any(Date),
+        retries: 1,
+      };
+
+      const result = await store.upsertOneFail({ filename, statistics, retries: 1 });
+      expect(result._id).toBeDefined();
+      expect(result).toMatchObject(expectedResult);
     });
 
-    test('should update one document in success collection', async () => {
+    test('should upsert (update) one document in success collection', async () => {
       const filename = '2020-08-25';
       const statistics = {
         gnuplot: '128',
@@ -98,15 +118,41 @@ describe('Store component tests', () => {
         filename,
         statistics,
         date: expect.any(Date),
-        retries: 1,
+        retries: 2,
       };
 
       const savedResult = await store.insertOneSuccess({
         filename,
         statistics,
+        retries: 1,
       });
 
-      const result = await store.updateOneSuccess({ ...savedResult, retries: 1 });
+      const result = await store.upsertOneSuccess({ ...savedResult, retries: 2 });
+      expect(result._id).toBeDefined();
+      expect(result).toMatchObject(expectedResult);
+    });
+
+    test('should upsert (update) one document in fail collection', async () => {
+      const filename = '2020-08-25';
+      const statistics = {
+        gnuplot: '128',
+        screenshots: '100',
+        stats: '80',
+      };
+      const expectedResult = {
+        filename,
+        statistics,
+        date: expect.any(Date),
+        retries: 2,
+      };
+
+      const savedResult = await store.insertOneFail({
+        filename,
+        statistics,
+        retries: 1,
+      });
+
+      const result = await store.upsertOneFail({ ...savedResult, retries: 2 });
       expect(result._id).toBeDefined();
       expect(result).toMatchObject(expectedResult);
     });
@@ -163,6 +209,70 @@ describe('Store component tests', () => {
       const result = await store.getFailByFilename(filename);
       expect(result._id).toEqual(savedId);
       expect(result).toMatchObject(expectedResult);
+    });
+  });
+
+  describe('getByStatus tests', () => {
+    test('should not get no documents by status in success collection', async () => {
+      const status = 'missing';
+      const result = await store.getSuccessByStatus(status);
+      expect(result).toEqual([]);
+    });
+
+    test('should not get no documents by status in fail collection', async () => {
+      const status = 'missing';
+      const result = await store.getFailByStatus(status);
+      expect(result).toEqual([]);
+    });
+
+    test('should get a document by status in success collection', async () => {
+      const status = 'missing';
+      const statistics = {
+        gnuplot: '128',
+        screenshots: '100',
+        stats: '80',
+      };
+      const expectedResult = {
+        status,
+        statistics,
+        date: expect.any(Date),
+      };
+
+      await store.insertOneSuccess({ status, statistics });
+      await store.insertOneSuccess({ status, statistics });
+      await store.insertOneSuccess({ status: 'sent', statistics });
+
+      const results = await store.getSuccessByStatus(status);
+      expect(results).toHaveLength(2);
+      results.forEach(result => {
+        expect(result._id).toBeDefined();
+        expect(result).toMatchObject(expectedResult);
+      });
+    });
+
+    test('should get a document by status in fail collection', async () => {
+      const status = 'missing';
+      const statistics = {
+        gnuplot: '128',
+        screenshots: '100',
+        stats: '80',
+      };
+      const expectedResult = {
+        status,
+        statistics,
+        date: expect.any(Date),
+      };
+
+      await store.insertOneFail({ status, statistics });
+      await store.insertOneFail({ status, statistics });
+      await store.insertOneFail({ status: 'sent', statistics });
+
+      const results = await store.getFailByStatus(status);
+      expect(results).toHaveLength(2);
+      results.forEach(result => {
+        expect(result._id).toBeDefined();
+        expect(result).toMatchObject(expectedResult);
+      });
     });
   });
 
@@ -253,6 +363,46 @@ describe('Store component tests', () => {
       await store.insertOneFail({ filename: filename2, statistics });
 
       const result = await store.deleteOneFailByFilename(filename1);
+      expect(result).toBe(1);
+
+      const remainingDocuments = await store.getAllFail();
+      expect(remainingDocuments).toHaveLength(1);
+    });
+  });
+
+  describe('deleteOne tests', () => {
+    test('should delete one document in success collection', async () => {
+      const filename1 = '2020-08-25';
+      const status = 'missing';
+      const statistics = {
+        gnuplot: '128',
+        screenshots: '100',
+        stats: '80',
+      };
+
+      const savedDoc = await store.insertOneSuccess({ filename: filename1, statistics, status });
+      await store.insertOneSuccess({ filename: filename1, statistics, status: 'sent' });
+
+      const result = await store.deleteOneSuccess(savedDoc);
+      expect(result).toBe(1);
+
+      const remainingDocuments = await store.getAllSuccess();
+      expect(remainingDocuments).toHaveLength(1);
+    });
+
+    test('should delete one document in fail collection', async () => {
+      const filename1 = '2020-08-25';
+      const status = 'missing';
+      const statistics = {
+        gnuplot: '128',
+        screenshots: '100',
+        stats: '80',
+      };
+
+      const savedDoc = await store.insertOneFail({ filename: filename1, statistics, status });
+      await store.insertOneFail({ filename: filename1, statistics, status: 'sent' });
+
+      const result = await store.deleteOneFail(savedDoc);
       expect(result).toBe(1);
 
       const remainingDocuments = await store.getAllFail();
