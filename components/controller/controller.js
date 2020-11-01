57761c2180dc367f8004e934e8d18f92caa0edcc
David Miguel Yusta <36865163+neodmy@users.noreply.github.com>
2020-11-01 21:56:48 +0100

    Feat/complete main controller (#30)
    * refactor: rename compressor and uploader controller
    
    * test: rename store tests
    
    * feat: rename init function, add functions for cronjob

---
+++ components/controller/controller.js
@@ -0,0 +1,64 @@
+const debug = require('debug')('service:task-controller');
+
+module.exports = () => {
+  const start = async ({
+    config, logger, archiver, compressor, uploader, store,
+  }) => {
+    debug('Initializing controller');
+    const { localPath } = config;
+
+    const init = async () => {
+      const filenames = await archiver.getDirectoryContent(localPath);
+
+      const processFilePromises = filenames.map(async filename => {
+        try {
+          const filenameWithoutExtension = filename.replace('.zip', '');
+          if (!filename.includes('zip')) await compressor.handleCompression(filenameWithoutExtension);
+          await uploader.handleUpload(filenameWithoutExtension);
+        } catch (error) {
+          logger.error(error.message);
+        }
+        Promise.resolve();
+      });
+
+      await Promise.all(processFilePromises);
+    };
+
+    const processFileToCompress = async filename => {
+      try {
+        await compressor.handleCompression(filename);
+        await uploader.handleUpload(filename);
+      } catch (error) {
+        logger.error(`Error compressing and uploading file | File ${filename}`);
+      }
+    };
+
+    const processFileToSend = async filename => {
+      try {
+        await uploader.handleUpload(filename);
+      } catch (error) {
+        logger.error(`Error uploading file | File ${filename}`);
+      }
+    };
+
+    const processRetries = async () => {
+      const filesToCompress = await store.getAll({ status: 'failed_to_compress' });
+      const filesToSend = await store.getAll({ status: 'failed_to_send' });
+
+      const processFilesToCompress = filesToCompress.map(({ filename }) => processFileToCompress(filename));
+
+      const processFilesToSend = filesToSend.map(({ filename }) => processFileToSend(filename));
+
+      await Promise.all(processFilesToCompress);
+      await Promise.all(processFilesToSend);
+    };
+
+    return {
+      init,
+      processFileToCompress,
+      processRetries,
+    };
+  };
+
+  return { start };
+};
--- test/components/controller/controller.test.js
+++ test/components/controller/controller.test.js
@@ -17,6 +17,7 @@ describe('Controller component tests', () => {
   let uploader;
   let compressor;
   let archiver;
+  let store;
 
   beforeAll(async () => {
     sys.set('slackBot', slackBotMock());
@@ -26,7 +27,7 @@ describe('Controller component tests', () => {
     sys.set('compressor', compressorMock());
     sys.set('uploader', uploaderMock());
     ({
-      controller, archiver, uploader, compressor,
+      controller, archiver, uploader, compressor, store,
     } = await sys.start());
   });
 
@@ -41,7 +42,7 @@ describe('Controller component tests', () => {
     return today.toISOString().split('T')[0];
   };
 
-  describe('handleTask', () => {
+  describe('init', () => {
     test('should handle two files to compress and upload', async () => {
       const filename = getFilename();
 
@@ -49,7 +50,7 @@ describe('Controller component tests', () => {
 
       let err;
       try {
-        await controller.handleTask();
+        await controller.init();
       } catch (error) {
         err = error;
       } finally {
@@ -70,7 +71,7 @@ describe('Controller component tests', () => {
 
       let err;
       try {
-        await controller.handleTask();
+        await controller.init();
       } catch (error) {
         err = error;
       } finally {
@@ -90,7 +91,7 @@ describe('Controller component tests', () => {
 
       let err;
       try {
-        await controller.handleTask();
+        await controller.init();
       } catch (error) {
         err = error;
       } finally {
@@ -112,7 +113,7 @@ describe('Controller component tests', () => {
 
       let err;
       try {
-        await controller.handleTask();
+        await controller.init();
       } catch (error) {
         err = error;
       } finally {
@@ -134,7 +135,7 @@ describe('Controller component tests', () => {
 
       let err;
       try {
-        await controller.handleTask();
+        await controller.init();
       } catch (error) {
         err = error;
       } finally {
@@ -144,6 +145,280 @@ describe('Controller component tests', () => {
 
         expect(uploader.handleUpload).toHaveBeenCalledTimes(2);
         expect(uploader.handleUpload).toHaveBeenCalledWith(filename);
+      }
+    });
+  });
+
+  describe('processFileToCompress', () => {
+    test('should compress and upload a file', async () => {
+      const filename = getFilename();
+
+      let err;
+      try {
+        await controller.processFileToCompress(filename);
+      } catch (error) {
+        err = error;
+      } finally {
+        expect(err).toBeUndefined();
+
+        expect(compressor.handleCompression).toHaveBeenCalledTimes(1);
+        expect(compressor.handleCompression).toHaveBeenCalledWith(filename);
+
+        expect(uploader.handleUpload).toHaveBeenCalledTimes(1);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(filename);
+      }
+    });
+
+    test('should handle a compress failure and not to try uploading the file', async () => {
+      const filename = getFilename();
+
+      compressor.handleCompression.mockRejectedValueOnce(new Error());
+
+      let err;
+      try {
+        await controller.processFileToCompress(filename);
+      } catch (error) {
+        err = error;
+      } finally {
+        expect(err).toBeUndefined();
+
+        expect(compressor.handleCompression).toHaveBeenCalledTimes(1);
+        expect(compressor.handleCompression).toHaveBeenCalledWith(filename);
+
+        expect(uploader.handleUpload).not.toHaveBeenCalled();
+      }
+    });
+
+    test('should compress the file and handle an upload failure', async () => {
+      const filename = getFilename();
+
+      uploader.handleUpload.mockRejectedValueOnce(new Error());
+
+      let err;
+      try {
+        await controller.processFileToCompress(filename);
+      } catch (error) {
+        err = error;
+      } finally {
+        expect(err).toBeUndefined();
+
+        expect(compressor.handleCompression).toHaveBeenCalledTimes(1);
+        expect(compressor.handleCompression).toHaveBeenCalledWith(filename);
+
+        expect(uploader.handleUpload).toHaveBeenCalledTimes(1);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(filename);
+      }
+    });
+  });
+
+  describe('processRetries', () => {
+    test('should compress and upload two files, and upload two other files', async () => {
+      const fileToCompress1 = {
+        filename: '2020-09-10',
+        status: 'failed_to_compress',
+        date: new Date().toISOString(),
+        retries: 1,
+      };
+
+      const fileToCompress2 = {
+        filename: '2020-09-11',
+        status: 'failed_to_compress',
+        date: new Date().toISOString(),
+        retries: 2,
+      };
+
+      const fileToUpload1 = {
+        filename: '2020-09-12',
+        status: 'failed_to_send',
+        date: new Date().toISOString(),
+        retries: 1,
+      };
+
+      const fileToUpload2 = {
+        filename: '2020-09-13',
+        status: 'failed_to_send',
+        date: new Date().toISOString(),
+        retries: 2,
+      };
+
+      store.getAll.mockResolvedValueOnce([fileToCompress1, fileToCompress2]);
+      store.getAll.mockResolvedValueOnce([fileToUpload1, fileToUpload2]);
+
+      let err;
+      try {
+        await controller.processRetries();
+      } catch (error) {
+        err = error;
+      } finally {
+        expect(err).toBeUndefined();
+
+        expect(compressor.handleCompression).toHaveBeenCalledTimes(2);
+        expect(compressor.handleCompression).toHaveBeenCalledWith(fileToCompress1.filename);
+        expect(compressor.handleCompression).toHaveBeenCalledWith(fileToCompress2.filename);
+
+        expect(uploader.handleUpload).toHaveBeenCalledTimes(4);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToCompress1.filename);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToCompress2.filename);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToUpload1.filename);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToUpload2.filename);
+      }
+    });
+
+    test('should handle a compress failure, compress and upload other file, and upload two other files', async () => {
+      const fileToCompress1 = {
+        filename: '2020-09-10',
+        status: 'failed_to_compress',
+        date: new Date().toISOString(),
+        retries: 1,
+      };
+
+      const fileToCompress2 = {
+        filename: '2020-09-11',
+        status: 'failed_to_compress',
+        date: new Date().toISOString(),
+        retries: 2,
+      };
+
+      const fileToUpload1 = {
+        filename: '2020-09-12',
+        status: 'failed_to_send',
+        date: new Date().toISOString(),
+        retries: 1,
+      };
+
+      const fileToUpload2 = {
+        filename: '2020-09-13',
+        status: 'failed_to_send',
+        date: new Date().toISOString(),
+        retries: 2,
+      };
+
+      store.getAll.mockResolvedValueOnce([fileToCompress1, fileToCompress2]);
+      store.getAll.mockResolvedValueOnce([fileToUpload1, fileToUpload2]);
+      compressor.handleCompression.mockRejectedValueOnce(new Error());
+
+      let err;
+      try {
+        await controller.processRetries();
+      } catch (error) {
+        err = error;
+      } finally {
+        expect(err).toBeUndefined();
+
+        expect(compressor.handleCompression).toHaveBeenCalledTimes(2);
+        expect(compressor.handleCompression).toHaveBeenCalledWith(fileToCompress1.filename);
+        expect(compressor.handleCompression).toHaveBeenCalledWith(fileToCompress2.filename);
+
+        expect(uploader.handleUpload).toHaveBeenCalledTimes(3);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToCompress2.filename);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToUpload1.filename);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToUpload2.filename);
+      }
+    });
+
+    test('should compress and upload a file, compress and handle an upload failure for other file, and upload two other files', async () => {
+      const fileToCompress1 = {
+        filename: '2020-09-10',
+        status: 'failed_to_compress',
+        date: new Date().toISOString(),
+        retries: 1,
+      };
+
+      const fileToCompress2 = {
+        filename: '2020-09-11',
+        status: 'failed_to_compress',
+        date: new Date().toISOString(),
+        retries: 2,
+      };
+
+      const fileToUpload1 = {
+        filename: '2020-09-12',
+        status: 'failed_to_send',
+        date: new Date().toISOString(),
+        retries: 1,
+      };
+
+      const fileToUpload2 = {
+        filename: '2020-09-13',
+        status: 'failed_to_send',
+        date: new Date().toISOString(),
+        retries: 2,
+      };
+
+      store.getAll.mockResolvedValueOnce([fileToCompress1, fileToCompress2]);
+      store.getAll.mockResolvedValueOnce([fileToUpload1, fileToUpload2]);
+      uploader.handleUpload.mockRejectedValueOnce(new Error());
+
+      let err;
+      try {
+        await controller.processRetries();
+      } catch (error) {
+        err = error;
+      } finally {
+        expect(err).toBeUndefined();
+
+        expect(compressor.handleCompression).toHaveBeenCalledTimes(2);
+        expect(compressor.handleCompression).toHaveBeenCalledWith(fileToCompress1.filename);
+        expect(compressor.handleCompression).toHaveBeenCalledWith(fileToCompress2.filename);
+
+        expect(uploader.handleUpload).toHaveBeenCalledTimes(4);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToCompress1.filename);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToCompress2.filename);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToUpload1.filename);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToUpload2.filename);
+      }
+    });
+
+    test('should compress and upload two files, upload other file, and handle an upload failure for other file', async () => {
+      const fileToCompress1 = {
+        filename: '2020-09-10',
+        status: 'failed_to_compress',
+        date: new Date().toISOString(),
+        retries: 1,
+      };
+
+      const fileToCompress2 = {
+        filename: '2020-09-11',
+        status: 'failed_to_compress',
+        date: new Date().toISOString(),
+        retries: 2,
+      };
+
+      const fileToUpload1 = {
+        filename: '2020-09-12',
+        status: 'failed_to_send',
+        date: new Date().toISOString(),
+        retries: 1,
+      };
+
+      const fileToUpload2 = {
+        filename: '2020-09-13',
+        status: 'failed_to_send',
+        date: new Date().toISOString(),
+        retries: 2,
+      };
+
+      store.getAll.mockResolvedValueOnce([fileToCompress1, fileToCompress2]);
+      store.getAll.mockResolvedValueOnce([fileToUpload1, fileToUpload2]);
+      uploader.handleUpload.mockRejectedValueOnce(new Error());
+
+      let err;
+      try {
+        await controller.processRetries();
+      } catch (error) {
+        err = error;
+      } finally {
+        expect(err).toBeUndefined();
+
+        expect(compressor.handleCompression).toHaveBeenCalledTimes(2);
+        expect(compressor.handleCompression).toHaveBeenCalledWith(fileToCompress1.filename);
+        expect(compressor.handleCompression).toHaveBeenCalledWith(fileToCompress2.filename);
+
+        expect(uploader.handleUpload).toHaveBeenCalledTimes(4);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToCompress1.filename);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToCompress2.filename);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToUpload1.filename);
+        expect(uploader.handleUpload).toHaveBeenCalledWith(fileToUpload2.filename);
       }
     });
   });
