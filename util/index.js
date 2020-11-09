2b3a8706dba76a069a5a743cd10c2bac0ef1e2f9
David Yusta <david.yusta@guidesmiths.com>
2020-11-09 19:07:46 +0100

    feat: add function to format date from CSV file

--- util/index.js
+++ util/index.js
@@ -6,6 +6,36 @@ const shouldRemove = (filename, removalOffset) => {
   return removalDate > fileDate;
 };
 
+// input format: mié. sept. 9 2020 or other string
+const formatDate = input => {
+  const monthMapping = {
+    ene: '01',
+    feb: '02',
+    mar: '03',
+    abr: '04',
+    may: '05',
+    jun: '06',
+    jul: '07',
+    ago: '08',
+    sept: '09',
+    oct: '10',
+    nov: '11',
+    dic: '12',
+  };
+
+  const firstSplit = (input && input.split('. ')) || [];
+  let formattedDate;
+  if (firstSplit.length === 3) {
+    const month = firstSplit[1];
+    const monthToDigitString = monthMapping[month];
+    const [day, year] = firstSplit[2].split(' ');
+    const formatDay = day < 10 ? `0${day}` : `${day}`;
+    formattedDate = [year, monthToDigitString, formatDay].join('-');
+  }
+  return formattedDate;
+};
+
 module.exports = {
   shouldRemove,
+  formatDate,
 };
