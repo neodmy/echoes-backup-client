05e1795f1890ac419ab504c6ccc1e04e125b3dd4
David Yusta <david.yusta@guidesmiths.com>
2021-01-10 13:03:09 +0100

    refactor: remove compressor

--- components/controller/index.js
+++ components/controller/index.js
@@ -1,12 +1,9 @@
 const System = require('systemic');
-const initCompressor = require('./compressor');
 const initUploader = require('./uploader');
 const initCsvController = require('./csv');
 const initController = require('./controller');
 
 module.exports = new System({ name: 'controller' })
-  .add('compressor', initCompressor())
-  .dependsOn({ component: 'config', source: 'controller' }, 'logger', 'archiver', 'store', 'slack')
   .add('uploader', initUploader())
   .dependsOn({ component: 'config', source: 'controller' }, 'logger', 'archiver', 'store', 'slack', 'sftp', 'mailer')
   .add('csv', initCsvController())
